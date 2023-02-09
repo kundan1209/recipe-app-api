07683b74e65f9f3dfa15b1264a36e4eb5c46d18c
KUNDAN SINGH <kundansingh@KUNDANs-MacBook-Air.local>
2023-02-09 08:49:02 +0530

    wait for db completed

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -5,6 +5,7 @@ from django.db.utils import OperationalError
 from django.core.management import call_command
 from django.test import SimpleTestCase
 
+
 @patch("core.management.commands.wait_for_db.Command.check")
 class CommandTests(SimpleTestCase):
     """test commands."""
@@ -13,4 +14,14 @@ class CommandTests(SimpleTestCase):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
         call_command('wait_for_db')
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """test waiting for db when getting operational error"""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
