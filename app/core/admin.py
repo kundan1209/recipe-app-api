7d9a1151b9179e0f6c3df435627dedc1b07deb93
KUNDAN SINGH <kundansingh@KUNDANs-MacBook-Air.local>
2023-02-17 00:58:23 +0530

    checking custom admin1

--- app/core/admin.py
+++ app/core/admin.py
@@ -21,7 +21,7 @@ class UserAdmin(BaseUserAdmin):
                     'is_superuser',
                 )
             }
-        )
+        ),
         (_('Important dates'),{'fields': ('last login',)}),
     )
 admin.site.register(models.User, UserAdmin)
